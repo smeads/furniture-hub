55d9641a34600835328e66200beda7f6417d0708
Steven Meads <sbmeads@gmail.com>
2016-11-15 13:58:54 -0800

    Add first feature

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # gem 'capistrano-rails', group: :development
 gem 'sass', '~> 3.4', '>= 3.4.22'
 gem 'bower-rails'
+gem 'angular-rails-templates'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,2 +1,17 @@
 angular
-  .module('starterApp', [])
+  .module('starterApp', ['ui.router', 'templates'])
+  .config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+    .state('/', {
+      url: '/index',
+      templateUrl: 'index.html',
+      controller: 'MainCtrl',
+    });
+
+    $urlRouterProvider.otherwise('index');
+
+  }]);
